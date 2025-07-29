c5964527db9ebb6c90e70be3f453aaa1aed82027
shyamalraju <shyamalraju@users.noreply.github.com>
2025-07-29 20:20:35 +0000

    [create-pull-request] automated change

--- tests/e2e/new-todo.spec.js
+++ tests/e2e/new-todo.spec.js
@@ -20,7 +20,7 @@ describe('New todo', () => {
 
   it('should create new todo', () => {
     cy.visit('/');
-    cy.contains('h1', 'todos');
+    cy.contains('h1', "todo's");
 
     cy.get('.new-todo')
       .type('Demo')
